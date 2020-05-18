2d578f93954be594e0badd8d59dec62b7bc877ba
Md Hasan <hasan.mmd28@gmail.com>
2020-05-18 01:18:17 -0400

    Frequency of Elements in a list (Not)Using Collections

--- InterviewQuestion/src/com/consultAdd/CountFrequency.java
+++ InterviewQuestion/src/com/consultAdd/CountFrequency.java
@@ -6,13 +6,16 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-
+/*
+ * Frequency of elements in a list
+ */
 
 public class CountFrequency {
 	
 
 		public static void main(String[] args) {
 			ArrayList<Integer> list= new ArrayList<Integer>();
+			ArrayList<String> list1= new ArrayList<String>();
 			list.add(1);//= {1,2,2,4,5,5,5,7};
 			list.add(1);
 			list.add(1);
@@ -35,13 +38,23 @@ public class CountFrequency {
 			list.add(7);
 			list.add(7);
 			list.add(1);
-			//list.sort(null);
-			countFreq(list);
-			
+			list1.add("hasan");
+			list1.add("hasan");
+			list1.add("kamal");
+			list1.add("hasan");
+			list1.add("hasan");
+			list1.add("moti");
+			list1.add("moti");
+			list1.add("hasan");
+			list1.add("John");
+			countFreq(list); // without using collections
+			countFreqInteger(list);// Using Collections Set and Map for Integer values
+			countFreqString(list1);	//Using Collections Set and Map for String values
 		}
 		
-		
-		/*public static void countFreq(ArrayList<Integer> list) {
+		//Without using Collections
+		public static void countFreq(ArrayList<Integer> list) {
+			list.sort(null);
 			int n= list.size();
 			int count=1;
 			int saveCount=0;
@@ -67,12 +80,10 @@ public class CountFrequency {
 	
 		}
 		
-		*/
+		// Using collections Map and Set
 		
-		
-		// Using collections
-		
-		public static void countFreq(ArrayList<Integer> list) {
+		//for Integers
+		public static void countFreqInteger(ArrayList<Integer> list) {
 			int n= list.size();
 			int count=1;
 			Set<Integer> s= new HashSet<Integer>();
@@ -89,11 +100,39 @@ public class CountFrequency {
 				}
 					
 			}
+			System.out.println("\n=============For Integer================");
 			for (Entry<Integer, Integer> entry : map.entrySet()) {
 			    Integer key = entry.getKey();
 			    Integer value = entry.getValue();
-			    System.out.println("key, " + key + " value " + value);
+			    System.out.println(key + " appears " + value+ " times.");
 			}
 
 		}
+		
+		//for String
+				public static void countFreqString(ArrayList<String> list) {
+					int n= list.size();
+					int count=1;
+					Set<String> s= new HashSet<String>();
+					Map<String,Integer> map=new HashMap<String,Integer>();
+					for(int i=0;i<n;i++) {
+						if(s.contains(list.get(i))) {
+							count++;
+							map.put(list.get(i), count);
+						}
+						else {
+							s.add(list.get(i));
+							count=1;
+							map.put(list.get(i), count);
+						}
+							
+					}
+					System.out.println("\n=============For String================");
+					for (Entry<String, Integer> entry : map.entrySet()) {
+					    String key = entry.getKey();
+					    Integer value = entry.getValue();
+					    System.out.println(key + " appears " + value+ " times.");
+					}
+
+				}
 }
