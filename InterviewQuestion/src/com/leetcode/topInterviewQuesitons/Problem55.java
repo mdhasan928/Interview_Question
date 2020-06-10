39af76604765225f3549a2b3cbf290bfe7522c83
Md Hasan <hasan.mmd28@gmail.com>
2020-06-10 03:36:37 -0400

    Jump Game using greedy algorithm

--- InterviewQuestion/src/com/leetcode/topInterviewQuesitons/Problem55.java
+++ InterviewQuestion/src/com/leetcode/topInterviewQuesitons/Problem55.java
@@ -85,13 +85,13 @@ public class Problem55
 	     */
 	    
 	        public static boolean canJump(int[] nums) {
-	            int lastPos = nums.length - 1;
+	            int lastPosIndex = nums.length - 1;
 	            for (int i = nums.length - 1; i >= 0; i--) {
-	                if (i + nums[i] >= lastPos) {
-	                    lastPos = i;
+	                if (i + nums[i] >= lastPosIndex) {
+	                    lastPosIndex = i;
 	                }
 	            }
-	            return lastPos == 0;
+	            return lastPosIndex == 0;
 	        }
 	   
 
