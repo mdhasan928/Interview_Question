3ff076e2d58015916bc06751ff09c631ecff87c0
Md Hasan <hasan.mmd28@gmail.com>
2020-06-12 18:49:35 -0400

    Merge Sorted Array start from m and n and decrease

---
+++ InterviewQuestion/src/com/leetcode/topInterviewQuesitons/Problem88.java
@@ -0,0 +1,60 @@
+package com.leetcode.topInterviewQuesitons;
+
+import java.util.Arrays;
+
+/*
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+Example:
+
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+Output: [1,2,2,3,5,6]
+ */
+public class Problem88 
+{
+	public static void main(String[] args)
+	{
+		int[] nums1= {1,3,7,0,0,0};
+		int[] nums2= {4,7,90};
+		int m=3,n=3;
+		mergeSortedArray(nums1,m,nums2,n);
+		System.out.println(Arrays.toString(nums1));
+	}
+	
+	public static void mergeSortedArray(int[]nums1, int m, int[] nums2, int n)
+	{
+		//Arrays.sort(nums1);
+		//Arrays.sort(nums1);
+		int i=m-1;
+		int j=n-1;
+		int k=nums1.length-1;
+		while(k>=0)
+		{
+			if(j<0)
+			{
+				return;
+			}
+		
+		if(i>=0&&nums1[i]>nums2[j])
+		{
+			nums1[k]=nums1[i];
+			i--;
+		}
+		else
+		{
+			nums1[k]=nums2[j];
+			j--;
+		}
+		k--;
+	}
+		
+    }
+
+}
