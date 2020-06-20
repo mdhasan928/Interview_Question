cf1fe95c9c2f788ea1658c8a7901f4c9dd3fb3c2
Md Hasan <hasan.mmd28@gmail.com>
2020-06-19 22:31:38 -0400

    Binary Tree Level Order Traversal iterative solution using queue.

--- InterviewQuestion/src/com/leetcode/topInterviewQuesitons51to100/Problem102.java
+++ InterviewQuestion/src/com/leetcode/topInterviewQuesitons51to100/Problem102.java
@@ -1,5 +1,61 @@
 package com.leetcode.topInterviewQuesitons51to100;
 
-public class Problem102 {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+ */
+
+public class Problem102 
+{
+	
+	public static List<List<Integer>> leveOrder(TreeNode root)
+	{
+		List<List<Integer>> result =new ArrayList<>();
+		if(root==null)
+		{
+			return result;
+		}
+		Queue<TreeNode> que=new LinkedList<>();
+		que.offer(root);
+		while(!que.isEmpty())
+		{
+			int queSize=que.size();
+			List<Integer> levelList=new ArrayList<>();
+			for(int i=0;i<queSize;i++)
+			{
+				TreeNode currentNode=que.poll();
+				levelList.add(currentNode.val);
+				if(currentNode.left!=null)
+				{
+					que.offer(currentNode.left);
+				}
+				if(currentNode.right!=null)
+				{
+					que.offer(currentNode.right);
+				}
+			}
+			result.add(levelList);
+			
+		}
+		return result;
+	}
 
 }
