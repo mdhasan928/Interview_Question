c1bcce5fb3cf0c17e59d40e6c04e7b47ba87fa96
Md Hasan <hasan.mmd28@gmail.com>
2020-06-22 09:45:27 -0400

    Divide Two Integers using while loop

--- InterviewQuestion/src/com/leetcode/topInterviewQuesitons/Problem29.java
+++ InterviewQuestion/src/com/leetcode/topInterviewQuesitons/Problem29.java
@@ -26,20 +26,84 @@ public class Problem29
 {
 	public static void main(String[] args) 
 	{
-		int dividend=258124694;
-		int divisor=-3;
+		int dividend=-2147483648;
+		int divisor=-1;
 		System.out.println(divide(dividend,divisor));
 	}
+	
+	 public static int divide(int dividend, int divisor) {
+	       
+	       
+	        //handle special cases
+	    if(divisor==0) return Integer.MAX_VALUE;
+	    if(divisor==-1 && dividend == Integer.MIN_VALUE)
+	        return Integer.MAX_VALUE;
+	 
+	    //get positive values
+	    long pDividend = Math.abs((long)dividend);
+	    long pDivisor = Math.abs((long)divisor);
+	 
+	    int result = 0;
+	    while(pDividend>=pDivisor){
+	        //calculate number of left shifts
+	        int numShift = 0;    
+	        while(pDividend>=(pDivisor<<numShift)){
+	            numShift++;
+	        }
+	 
+	        //dividend minus the largest shifted divisor
+	        result += 1<<(numShift-1);
+	        pDividend -= (pDivisor<<(numShift-1));
+	    }
+	 
+	    if((dividend>0 && divisor>0) || (dividend<0 && divisor<0)){
+	        return result;
+	    }else{
+	        return -result;
+	    }
+	        
+	      /* 
+	        //handle special cases
+	    if(divisor==0) return Integer.MAX_VALUE;
+	    if(divisor==-1 && dividend == Integer.MIN_VALUE)
+	        return Integer.MAX_VALUE;
+	        
+	        int sign1=1,sign2=1;
+	        if(dividend<0)
+	        {
+	            sign1=-1;
+	        }
+	        if(divisor<0)
+	        {
+	            sign2=-1;
+	        }
+	        int quotient=0;
+	        //get positive values
+	    long pdividend = Math.abs((long)dividend);
+	    long pdivisor = Math.abs((long)divisor);
+	        while(pdivisor<=pdividend)
+	        {
+	            quotient++;
+	            pdividend=pdividend-pdivisor;
+	            
+	        }
+	        return sign1*sign2*quotient;
+	       */ 
+	        
+	    }
+	
+	
+	/*
 	public static int divide(int dividend, int divisor)
 	{
 		if(divisor==0) {
 			return 0;
 		}
-		if(dividend<Integer.MIN_VALUE||dividend>Integer.MAX_VALUE)
+		if(dividend<=Integer.MIN_VALUE||dividend>=Integer.MAX_VALUE)
 		{
 			return Integer.MAX_VALUE;
 		}
-		if(divisor<Integer.MIN_VALUE||divisor>Integer.MAX_VALUE)
+		if(divisor<=Integer.MIN_VALUE||divisor>=Integer.MAX_VALUE)
 		{
 			return Integer.MAX_VALUE;
 		}
@@ -58,7 +122,8 @@ public class Problem29
 		
 		return sign1*sign2*quotient;
 	}
-
+	*/
+   
 }
 
 
