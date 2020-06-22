8a559339d19713e9fd8415f90c081ac1b04b25de
Md Hasan <hasan.mmd28@gmail.com>
2020-06-22 09:44:56 -0400

    Symmetric Tree both recursive/iterative

--- InterviewQuestion/src/com/leetcode/topInterviewQuesitons51to100/Problem101.java
+++ InterviewQuestion/src/com/leetcode/topInterviewQuesitons51to100/Problem101.java
@@ -25,12 +25,22 @@ But the following [1,2,2,null,3,null,3] is not:
  */
 public class Problem101 
 {
-	/*
-	//recursive
+	
 	public static void main(String[] args)
 	{
+		TreeNode root=new TreeNode(1);
+		root.left=new TreeNode(2);
+		root.right=new TreeNode(2);
 		
+		root.right.left=new TreeNode(4);
+		root.right.right=new TreeNode(3);
+		root.left.left=new TreeNode(3);
+		root.left.right=new TreeNode(4);
+		System.out.println(isSymmetric(root));
 	}
+	/*
+	//recursive
+	
 	public static boolean isSymmetric(TreeNode root)
 	{
 		if(root==null)
@@ -53,48 +63,53 @@ public class Problem101
 		
 		return isSymmetric(left.left,right.right)&&isSymmetric(left.right,right.left);
 		
-	}*/
+	}
+	*/
+	
 	
 	//iterative
 	
-	public static boolean isSymmetric(TreeNode root)
-	{
-		if(root==null)
+	 public static boolean isSymmetric(TreeNode root)
 		{
+			if(root==null)
+			{
+				return true;
+			}
+			Queue<TreeNode> q1=new LinkedList<>();// for left subtree
+			Queue<TreeNode> q2=new LinkedList<>();//for right subtree
+			q1.offer(root.left);
+			q2.offer(root.right);
+	        
+			while(!q1.isEmpty() && !q2.isEmpty())
+			{
+				TreeNode n1=q1.poll();
+				TreeNode n2=q2.poll();
+				if((n1==null&&n2!=null)||(n1!=null&&n2==null))
+	            {
+					return false;
+	            }
+	            if(n1!=null)
+	            {
+				    if(n1.val!=n2.val)
+	                {
+					   return false;
+	                }
+				    q1.offer(n1.left);
+			    	q1.offer(n1.right);
+			    	q2.offer(n2.right);
+			    	q2.offer(n2.left);
+	            }
+				
+			}
 			return true;
 		}
-		Queue<TreeNode> q1=new LinkedList<>();// for left subtree
-		Queue<TreeNode> q2=new LinkedList<>();//for right subtree
-		q1.offer(root.left);
-		q2.offer(root.right);
-		while(!q1.isEmpty()&&!q2.isEmpty())
-		{
-			TreeNode n1=q1.poll();
-			TreeNode n2=q2.poll();
-			if(n1==null&&n2!=null||n1!=null&&n2==null)
-			{
-				return false;
-			}
-			if(n1.val!=n2.val)
-			{
-				return false;
-			}
-			q1.offer(n1.left);
-			q1.offer(n1.right);
-			
-			q2.offer(n2.left);
-			q2.offer(n2.right);
-			
-		}
-		return true;
-	}
-	
+
 
 }
 
 class TreeNode
 {
-	int val;
+	Integer val;
 	TreeNode left;
 	TreeNode right;
 	
@@ -106,9 +121,9 @@ class TreeNode
 		this.val = val;
 	}
 
-	public TreeNode(int val) {
+	public TreeNode(Integer val) {
 		super();
 		this.val = val;
 	}
-	
+
 }
