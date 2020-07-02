392ae1738e18f9e79b03fe4d7e101411ea45ae50
Md Hasan <hasan.mmd28@gmail.com>
2020-07-02 00:20:40 -0400

    Best Time to Buy and Sell Stock II

---
+++ InterviewQuestion/src/com/leetcode/topInterviewQuesitons51to100/Problem122.java
@@ -0,0 +1,49 @@
+package com.leetcode.topInterviewQuesitons51to100;
+/*
+ * Say you have an array prices for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+             Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Example 2:
+
+Input: [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+             Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+             engaging multiple transactions at the same time. You must sell before buying again.
+Example 3:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ */
+public class Problem122 {
+	
+	public static void main(String[] args) {
+		int[] prices= {7,1,5,3,6,4};
+		System.out.println(maxProfit(prices));
+	}
+
+	public static int maxProfit(int[] prices) {
+		int min_val=Integer.MAX_VALUE;
+		int profit=0;
+		for(int i=0;i<prices.length;i++) {
+			if(prices[i]<min_val) {
+				min_val=prices[i];
+			}
+			else {
+				profit+=prices[i]-min_val;
+				min_val=prices[i];
+			}
+		}
+		return profit;
+	}
+}
